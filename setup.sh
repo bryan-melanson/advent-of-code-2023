9c1e416cd8eff22b2f4ad2a6556b84327a152c1e
Bryan Melanson <hello@bryan.horse>
2023-12-01 11:52:49 -0500

    Fix colors

--- setup.sh
+++ setup.sh
@@ -1,35 +1,34 @@
 #!/bin/bash
 
-RED='\e[0;31m'
-GREEN='\e[0;32m'
-NC='\e[0m' # No Color
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-PS3="Select a programming language:"
+PS3="Select a programming language: "
 
-select dir in python rust go zig nim
-do
-    if [[ ! -d $dir ]]; then
-        mkdir $dir
-        cp template/$dir.gitignore $dir/.gitignore
-    fi
-    if [[ ! -d template/$dir ]]; then
-        echo -e "${RED}Template doesn't exist${NC}"
-        exit
-    fi
-    echo -e "${GREEN}OK!${NC}"
-    break
+select dir in python rust go zig nim; do
+	if [[ ! -d $dir ]]; then
+		mkdir $dir
+		cp template/$dir.gitignore $dir/.gitignore
+	fi
+	if [[ ! -d template/$dir ]]; then
+		echo -e "${RED}Template doesn't exist${NC}"
+		exit
+	fi
+	echo -e "${GREEN}OK!${NC}"
+	break
 done
 
 read -p "Enter the day number to set up the files:" day
 
 if [[ $day -eq 0 || $day -gt 25 ]]; then
-    echo -e "${RED}Enter a number between 1 and 25${NC}"
+	echo -e "${RED}Enter a number between 1 and 25${NC}"
 else
-    if [ $day -le 9 ]; then
-        day=0$day
-    fi
-    if [[ ! -d $dir/$day ]]; then
-        cp -rf template/$dir $dir/$day
-    fi
-    echo -e "${GREEN}OK!${NC}"
+	if [ $day -le 9 ]; then
+		day=0$day
+	fi
+	if [[ ! -d $dir/$day ]]; then
+		cp -rf template/$dir $dir/$day
+	fi
+	echo -e "${GREEN}OK!${NC}"
 fi
