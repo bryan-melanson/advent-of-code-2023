75f2aa3389160f5149d3239799634509072a7453
Bryan Melanson <hello@bryan.horse>
2023-12-02 16:50:03 -0500

    Update setup scripts

--- template/python/part1.py
+++ template/python/part1.py
@@ -2,14 +2,6 @@ import time
 
 
 def part1(x):
+    total = 0
     with open(x, "r") as f:
         return f.read().splitlines()
-
-
-test_val = 0
-if part1("test1") == test_val:
-    start_time = time.time()
-    print('Solution is {}'.format(part1("input")))
-    print("--- %s seconds ---" % (time.time() - start_time))
-else:
-    print("Test failed")
