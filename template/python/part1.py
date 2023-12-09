59f1a6a6b55291b48718efa3f8a8241f3a593073
Bryan Melanson <hello@bryan.horse>
2023-12-09 13:48:06 -0500

    Fix python templates

--- template/python/part1.py
+++ template/python/part1.py
@@ -1,8 +1,5 @@
-import time
-
-
 def part1(x):
-    total = 0
+    total = -1
     with open(x, "r") as f:
         for line in f.read().splitlines():
             print(line)
--- template/python/part2.py
+++ template/python/part2.py
@@ -1,7 +1,5 @@
-import time
-
-
 def part2(x):
+    total = -1
     with open(x, "r") as f:
         for line in f.read().splitlines():
             print(line)
